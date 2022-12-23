eaba123326b2ab154cecbad30faf860520541b2d
hiromikoreyes <114784445+hiromikoreyes@users.noreply.github.com>
2022-12-23 11:34:48 -0500

    implemented playRound(), compareChoices(), game()

--- app.js
+++ app.js
@@ -33,9 +33,58 @@ function getComputerChoice(){
     }
 }
 
+
 function getPlayerChoice(){
     return prompt("what do you choose?: ").toLowerCase();
 }
+
+
+function compareChoices(player,computer){
+    //Return the winner of the round or a tie
+    if (player == computer){
+        return 'tie';
+    }
+
+    if (player == 'rock' && computer == 'scissors' ||
+    player == 'paper' && computer == 'rock' ||
+    player == 'scissors' && computer == 'paper'){
+        return 'player';
+    }
+    return 'computer';
+}
+
+
+function playRound(){
+    /*Return a string message detailing the game choices and the outcome of
+    the game*/
+    let playerChoice = getPlayerChoice();
+    let computerChoice = getComputerChoice();
+    let outcome = compareChoices(playerChoice,computerChoice);
+
+
+
+    switch(outcome){
+        case 'tie':
+            return 'It was a tie, both played ' + playerChoice;
+            break;
+        case 'player':
+            return 'You won!!!1, you played: ' + playerChoice + ' computer played: ' + computerChoice;
+            break;
+        case 'player':
+            return 'You lost! :((( , you played: ' + playerChoice + ' computer played: ' + computerChoice;
+            break;
+
+    }
+
+
+}
+
+function game(){
+    for (i = 0; i < 3; i++){
+        playRound()
+    }
+}
+
 
 
 
