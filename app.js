b4b5025af52d3e835c223dc48babe84e833e1726
hiromikoreyes <114784445+hiromikoreyes@users.noreply.github.com>
2022-12-23 11:16:25 -0500

    implemented getComputerChoice

---
+++ app.js
@@ -0,0 +1,39 @@
+/*
+
+functions to work on:
+
+getComputerChoice
+
+
+getPlayerChoice
+
+
+compareChoices
+
+
+playRound
+
+*/
+
+function getComputerChoice(){
+    //Return the play of the computer in the game.
+
+    let random = Math.floor(Math.random() * 2);
+
+    switch(random){
+        case 0:
+            return 'rock';
+            break;
+        case 1:
+            return 'paper';
+            break;
+        case 2:
+            return 'scissors';
+            break;
+    }
+
+
+
+    
+
+}
